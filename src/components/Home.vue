454a4f85493bedf58892c2f197c7133fb3fc237b
Joshua Angelo Bienes <eyedeceiver03@gmail.com>
2017-08-11 23:08:45 +0800

    added Sign Up to firebase authentication, implemented vue-router, refactored navbar and landing page for SPA to take effect.

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <button @click="logoutUser">Log Out</button>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    methods: {
+      logoutUser(){
+//        firebase.auth().signOut().then(() => {
+//          console.log('successful')
+//          this.$router.push('/')
+//        }).catch(err => {
+//          console.log(err)
+//        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
