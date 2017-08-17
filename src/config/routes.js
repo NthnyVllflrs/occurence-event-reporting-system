c8af528737b05d41e1137153fab81a024888df78
Joshua Angelo Bienes <eyedeceiver03@gmail.com>
2017-08-17 13:32:16 +0800

    changed route to /home/add

--- src/config/routes.js
+++ src/config/routes.js
@@ -19,7 +19,7 @@ const routes = [
     }
   },
   {
-    path: '/home/AddOccurrence',
+    path: '/home/add',
     component: AddOccurrence,
     beforeEnter: (to, from, next) => {
       if(!store.getters.currentUser){
