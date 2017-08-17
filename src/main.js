e1b5de4864513805cb9a9b3bdca85682cd161087
Joshua Angelo Bienes <eyedeceiver03@gmail.com>
2017-08-18 01:02:15 +0800

    User can now post event but other fields still doesn't work. Worked with moment.js to display time post

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import * as firebase from 'firebase'
+import './config/firebase'
+import VueFire from 'vuefire'
 
 Vue.use(VueRouter)
+Vue.use(VueFire)
 
 import {store} from "./config/store"
 import {router} from "./config/routes"
@@ -12,17 +14,7 @@ new Vue({
   el: '#app',
   render: h => h(App),
   router,
-  store,
-  created(){
-    firebase.initializeApp({
-      apiKey: "AIzaSyDrBu60WVv5FNwLcsCTbuB2Pb2FQuIK5eM",
-      authDomain: "occurence-df954.firebaseapp.com",
-      databaseURL: "https://occurence-df954.firebaseio.com",
-      projectId: "occurence-df954",
-      storageBucket: "",
-      messagingSenderId: "779704581019"
-    })
-  }
+  store
   // beforeCreate: () => {
   //   firebase.auth().onAuthStateChanged(user => {
   //     console.log(user)
