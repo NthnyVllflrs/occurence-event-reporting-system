6ba9935598c487beeaebbee1bcff6f10de6091e6
lix <lixsalvador18@gmail.com>
2017-08-21 11:38:50 +0800

    For edit profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,69 @@
+<template>
+    <div style = "padding-top: 72px">
+        <section class="hero is-bold">
+
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns is-vcentered">
+                        <div class="column is-4 is-offset-4">
+                            <h1 class="title">
+                                Edit Profile
+                            </h1>
+
+                            <div class="box">
+                                <div class="field">
+                                    <div class="file is-small has-name is-fullwidth">
+                                            <input class="file-input" type="file" id="file" accept=".jpg, .jpeg, .png" @change="getFileName">
+                                                <span class="file-cta">
+                                                    <span class="file-label">
+                                                    Change Image
+                                                    </span>
+                                                </span>
+                                                <span class="file-name">
+                                                     {{ fileName }}
+                                                </span>
+                                    </div>
+                                </div>
+
+                                    <div class="field">
+                                         <p class="control has-icons-left">
+                                            <input class="input" type="text" placeholder="First Name" v-model="description">
+                                                <span class="icon is-small is-left">
+                                                    <i class="fa fa-pencil-square-o"></i>
+                                                </span>
+                                         </p>
+                                    </div>
+
+                                    <div class="field">
+                                         <p class="control has-icons-left">
+                                            <input class="input" type="text" placeholder="Last Name" v-model="description">
+                                                <span class="icon is-small is-left">
+                                                    <i class="fa fa-pencil-square-o"></i>
+                                                </span>
+                                         </p>
+                                    </div>
+
+                                    <hr>
+                                        <div class="level">
+                                            <div class="level-left"></div>
+                                                <div class="level-right">
+                                                    <p class="level-item">
+                                                        <a class="button is-primary" @click="postEvent">
+                                                            <span>Accept</span>
+                                                        </a>
+                                                    </p>
+                                                </div>
+                                        </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </div>
+</template>
+
+<style scoped>
+@import "../assets/css/timeline.css";
+</style>
--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router'
 import LandingPage from '../components/LandingPage.vue'
 import Home from '../components/Home.vue'
 import AddOccurrence from '../components/AddOccurrence.vue'
+import Profile from '../components/Profile.vue'
 import toastr from 'toastr'
 
 import {store} from "./store"
@@ -33,6 +34,18 @@ const routes = [
   {
     path: '/home',
     component: Home,
+    beforeEnter: (to, from, next) => {
+      if(!store.getters.currentUser){
+        toastr.error('You are not yet logged in')
+        next('/')
+      } else {
+        next()
+      }
+    }
+  },
+  {
+    path: '/profile',
+    component: Profile,
     beforeEnter: (to, from, next) => {
       if(!store.getters.currentUser){
         toastr.error('You are not yet logged in')
